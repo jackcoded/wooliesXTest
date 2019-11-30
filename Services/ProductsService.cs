28b834222cb70d4095b403b99d26a3f2105f670f
jack <jackisalright@gmail.com>
2019-11-30 12:50:28 +1100

    added sorts except recommended

--- Services/ProductsService.cs
+++ Services/ProductsService.cs
@@ -25,9 +25,30 @@ namespace wooliesXTest.Services
         }
        public async Task<List<Product>> SortProducts(string sortOptions)
         {
-            return await GetData<List<Product>>("products");
+            List<Product> productList = await GetData<List<Product>>("products");
+            List<Order> recommendedList;
+            List<Order> orderList = await GetData<List<Order>>("shopperHistory");
+
+            if (String.Equals(sortOptions, "low", StringComparison.InvariantCultureIgnoreCase)) {
+                productList = productList.OrderBy(p => p.Price).ToList();
+            }
+            else if (String.Equals(sortOptions, "high", StringComparison.InvariantCultureIgnoreCase)) {
+                productList = productList.OrderByDescending(p => p.Price).ToList();
+            }
+            else if (String.Equals(sortOptions, "ascending", StringComparison.InvariantCultureIgnoreCase)) {
+                productList = productList.OrderBy(p => p.Name).ToList();
+            }
+            else if (String.Equals(sortOptions, "descending", StringComparison.InvariantCultureIgnoreCase)) {
+                productList = productList.OrderByDescending(p => p.Name).ToList();
+            }
+            else if (String.Equals(sortOptions, "recommended", StringComparison.InvariantCultureIgnoreCase)) {
+                productList = orderList.Select(o => o.Products)
+                                       .SelectMany(p => p).ToList();
+            }
+            return productList;
         }
 
+        // Generic helper for Getting data and converting json
         private async Task<T> GetData<T>(string endPoint)
         {
             var token = _config["appSettings:appToken"];
