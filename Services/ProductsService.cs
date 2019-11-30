dcf7e16947276461dedeebdc9f64c5f607b74076
jack <jackisalright@gmail.com>
2019-11-30 12:14:48 +1100

    set up exercise 2 and trying to test woolies resource end point

---
+++ Services/ProductsService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wooliesXTest.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace wooliesXTest.Services
+{
+    public interface IProductsService
+    {
+        Task<List<Product>> SortProducts(string sortOptions);
+    }
+    public class ProductsService : IProductsService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+        public ProductsService(HttpClient httpClient, [FromServices] IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+        }
+       public async Task<List<Product>> SortProducts(string sortOptions)
+        {
+            return await GetData<List<Product>>("products");
+        }
+
+        private async Task<T> GetData<T>(string endPoint)
+        {
+            var token = _config["appSettings:appToken"];
+            var uri = _config["appSettings:appUri"];
+            var content = await _httpClient.GetStringAsync($"{uri}/resource/{endPoint}?token={token}");
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+    }
+}
