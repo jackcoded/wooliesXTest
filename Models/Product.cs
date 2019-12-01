eb494c838db78c1f23f642c08bbd3e41ebae100d
jack <jackisalright@gmail.com>
2019-12-01 13:17:44 +1100

    Revert "set up exercise 3 trolleyCalculator"
    This reverts commit 7b9c99378f7c40305e0a1b9ace71abe7129ee3dd.

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,16 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+
 namespace wooliesXTest.Models
 {
     public class Product
     {
-        [JsonProperty("name")]
         public string Name { get; set; }
-        [JsonProperty("price")]
         public decimal Price { get; set; }
-        [JsonProperty("quantity")]
         public decimal Quantity { get; set; }
     }
 }
